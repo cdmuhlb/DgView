14503ea9ab5fd905b42210c755fc7d35867b20d2
Curran D. Muhlberger <curran@muhlbergerweb.com>
2013-11-07 17:52:17 -0500

    Refactor ColorMaps and add BlackbodyColorMap
    ColorMapFactories provide a consistent construction interface, allowing
    ColorMaps to be constructed or copied without knowing the class at
    compile-time.  Note: this approach feels sloppy and will probably need
    to be refactored again in the future.
    
    Also add BlackBody ColorMap.  Unfortunately, it does not produce the
    desired look, probably because the fit disallows temperatures low enough
    to produce reds.

--- src/main/scala/cdmuhlb/dgview/ColorMap.scala
+++ src/main/scala/cdmuhlb/dgview/ColorMap.scala
@@ -4,6 +4,16 @@ import scala.math.{acos, atan2, cbrt, cos, pow, sin, sqrt}
 
 trait ColorMap {
   def map(z: Double): Int
+  def getFactory: ColorMapFactory
+}
+
+trait ColorMapFactory {
+  def createMap(lo: Double, hi: Double): ColorMap
+}
+
+object GammaGrayLinearFactory extends ColorMapFactory {
+  def createMap(lo: Double, hi: Double) = GammaGrayLinearColorMap(lo, hi)
+  override def toString(): String = "Grayscale"
 }
 
 case class GammaGrayLinearColorMap(lo: Double, hi: Double) extends ColorMap {
@@ -13,6 +23,56 @@ case class GammaGrayLinearColorMap(lo: Double, hi: Double) extends ColorMap {
     val v = math.rint(((z - lo)/(hi - lo)).max(0.0).min(1.0)*255.0).toInt
     (0xff<<24) | (v<<16) | (v<<8) | v
   }
+
+  def getFactory = GammaGrayLinearFactory
+}
+
+object BlackbodyFactory extends ColorMapFactory {
+  def createMap(lo: Double, hi: Double) = BlackbodyColorMap(lo, hi)
+  override def toString(): String = "Blackbody"
+}
+
+case class BlackbodyColorMap(lo: Double, hi: Double) extends ColorMap {
+  require(hi > lo)
+
+  def map(z: Double): Int = {
+    val tMax = 6500.0
+    val tMin = 1667.0
+    val t = ((z - lo)/(hi - lo)).max(0.0).min(1.0)*(tMax - tMin) + tMin
+    val invT = 1.0 / t
+    val invT2 = invT * invT
+    val invT3 = invT2 * invT
+    val x = if (t < 4000.0) {
+      -0.2661239e9*invT3 - 0.2343580e6*invT2 + 0.8776956e3*invT + 0.179910
+    } else {
+      -3.0258469e9*invT3 + 2.1070379e6*invT2 + 0.2226347e3*invT + 0.240390
+    }
+    val x2 = x * x
+    val x3 = x2 * x
+    val y = if (t < 2222.0) {
+      -1.1063814*x3 - 1.34811020*x2 + 2.18555832*x - 0.20219683
+    } else if (t < 4000.0f) {
+      -0.9549476*x3 - 1.37418593*x2 + 2.09137015*x - 0.16748867
+    } else {
+      3.0817580*x3 - 5.87338670*x2 + 3.75112997*x - 0.37001483
+    }
+
+    val xyzY = math.pow(t/tMax, 4).max(0.0).min(1.0)
+    val xyzX = (xyzY * x / y).max(0.0).min(1.0)
+    val xyzZ = (xyzY * (1.0 - x - y) / y).max(0.0).min(1.0)
+    val (cr, cg, cb) = ColorSpaceConversion.cieXyzToSRgb(
+        CieXyzColor(xyzX, xyzY, xyzZ)).toBytes
+    (0xff<<24) | (cr<<16) | (cg<<8) | cb
+  }
+
+  def getFactory = BlackbodyFactory
+}
+
+case class ContourLinearFactory(nContours: Int, map: ColorMap)
+    extends ColorMapFactory {
+  def createMap(lo: Double, hi: Double) =
+      ContourLinearColorMap(lo, hi, nContours, map)
+  override def toString(): String = "sContour($map)"
 }
 
 case class ContourLinearColorMap(lo: Double, hi: Double, nContours: Int,
@@ -29,6 +89,22 @@ case class ContourLinearColorMap(lo: Double, hi: Double, nContours: Int,
       map.map(lo + stripeNum*stripeWidth)
     }
   }
+
+  lazy val getFactory = ContourLinearFactory(nContours, map)
+
+  def withColormap(newMap: ColorMap): ContourLinearColorMap =
+      ContourLinearColorMap(lo, hi, nContours, newMap)
+
+  def withFactory(factory: ColorMapFactory): ContourLinearColorMap =
+      withColormap(factory.createMap(lo, hi))
+
+  def withContours(newContours: Int) =
+      ContourLinearColorMap(lo, hi, newContours, map)
+}
+
+object DivergingLinearFactory extends ColorMapFactory {
+  def createMap(lo: Double, hi: Double) = DivergingLinearColorMap(lo, hi)
+  override def toString(): String = "Diverging"
 }
 
 case class DivergingLinearColorMap(lo: Double, hi: Double) extends ColorMap {
@@ -41,6 +117,8 @@ case class DivergingLinearColorMap(lo: Double, hi: Double) extends ColorMap {
     val (cr, cg, cb) = ColorSpaceConversion.mshToSRgb(mshFunc(zNorm)).toBytes
     (0xff<<24) | (cr<<16) | (cg<<8) | cb
   }
+
+  def getFactory = DivergingLinearFactory
 }
 
 
